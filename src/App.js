3cebf22e2fab220f5cd8b4d0b6e2b6e13191fc7e
silviaMYA <s.mik@hotmail.com>
2022-05-05 13:21:20 +1000

    Organizing components in folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   //Integer value representing the month, beginning with 0 for January to 11 for December.
