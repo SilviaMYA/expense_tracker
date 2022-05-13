5a8bd3c8179951d473db0e255e04835e6d99a66c
silviaMYA <s.mik@hotmail.com>
2022-05-13 18:20:51 +1000

    Filtering expenses by year

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ const INITIAL_EXPENSES_ARRAY = [
     id: "expense2",
     title: "New TV",
     amount: 799.49,
-    date: new Date(2022, 2, 12), // 12-March-2022
+    date: new Date(2021, 2, 12), // 12-March-2022
   },
   {
     id: "expense1",
     title: "Easter Trip",
     amount: 2015.12,
-    date: new Date(2022, 3, 14), // 14-April-2022
+    date: new Date(2020, 3, 14), // 14-April-2022
   },
   {
     id: "expense3",
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,25 +2,41 @@ import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 /**
  *
  * @param {Object} props contains items that gets from App.js
  * @returns
  */
+
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (yearSelected) => {
+    setFilteredYear(yearSelected);
+  };
+
+  // filters expenses for the year selected
+  const filteredExpenses = props.items.filter((item) => {
+    return item.date.getFullYear().toString() === filteredYear;
+  });
+
+  let filteredContent = <p>No items found</p>;
+  if(filteredExpenses.length > 0) {
+    filteredContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
   return (
     <Card className="expenses">
-      <ExpensesFilter />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      ;
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filteredContent}
     </Card>
   );
 };
